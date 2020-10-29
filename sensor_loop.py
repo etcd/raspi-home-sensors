7502976b3e83dbeedc1444a3fd1b14eefbb3d23d
etcd <etcd@users.noreply.github.com>
2020-10-29 14:33:51 -0500

    Create sensor_loop.py

---
+++ sensor_loop.py
@@ -0,0 +1,14 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+sheet = client.open("Home Sensors").sheet1
+
+# Extract and print all of the values
+list_of_records = sheet.get_all_records()
+print(list_of_records)
