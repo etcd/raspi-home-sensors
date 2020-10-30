a7ca9755ce3168054b4abe87f847f32ba5953aef
etcd <etcd@users.noreply.github.com>
2020-10-30 12:27:22 -0500

    Update meta_loop.sh

--- code/meta_loop.sh
+++ code/meta_loop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# THIS FILE IS EXECUTED BY ROOT.
+# However, the command executed by this file is run by `pi`.
+#
 # Note: The [until loop](https://linuxize.com/post/bash-until-loop/) scaffolding
 # is a popular bash scripting pattern for restarting a program when it fails.
 # Also, this redirects any program output into `stdout.txt` and `stderr.txt` files
@@ -7,7 +10,7 @@
 
 counter=0
 
-until python3 sensor_loop.py >> stdout.txt 2> stderr.txt
+until su pi -c 'python3 sensor_loop.py >> stdout.txt 2> stderr.txt'
 do
     echo "Sensor loop failed (failure count since restart: $counter). Respawning script..."  >> stdout.txt
     sleep 10
