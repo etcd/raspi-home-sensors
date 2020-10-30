feab919cf322a3044b153d9c1d3d8c13c2b075ab
etcd <etcd@users.noreply.github.com>
2020-10-29 20:25:48 -0500

    Update meta_loop.sh

--- meta_loop.sh
+++ meta_loop.sh
@@ -3,7 +3,7 @@
 # Also, this redirects any program output into `stdout.txt` and `stderr.txt` files
 # for ease of debugging. 
 
-until [ sudo python3 /sensor_loop.py >> /stdout.txt 2> /stderr.txt ]
+until python3 /sensor_loop.py >> /stdout.txt 2> /stderr.txt
 do
     echo "Sensor loop failed. Respawning script..."  >> /stdout.txt
     sleep 10
