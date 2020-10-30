95c7ff61e62bab8b493348fc6dda00f280ba3cbd
etcd <etcd@users.noreply.github.com>
2020-10-29 21:32:56 -0500

    Update meta_loop.sh

--- meta_loop.sh
+++ meta_loop.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 # Note: The [until loop](https://linuxize.com/post/bash-until-loop/) scaffolding
 # is a popular bash scripting pattern for restarting a program when it fails.
 # Also, this redirects any program output into `stdout.txt` and `stderr.txt` files
 # for ease of debugging. 
 
+counter=0
+
 until python3 /sensor_loop.py >> /stdout.txt 2> /stderr.txt
 do
-    echo "Sensor loop failed. Respawning script..."  >> /stdout.txt
+    echo "Sensor loop failed (failure count since restart: $counter). Respawning script..."  >> /stdout.txt
     sleep 10
+    ((counter++))
 done
