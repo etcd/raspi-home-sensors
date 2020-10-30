f7f63b2495f8bc49d321cc05a1676b9212ee915e
etcd <etcd@users.noreply.github.com>
2020-10-29 20:16:04 -0500

    Update meta_loop.sh

--- meta_loop.sh
+++ meta_loop.sh
@@ -1,4 +1,7 @@
-# 
+# Note: The [until loop](https://linuxize.com/post/bash-until-loop/) scaffolding
+# is a popular bash scripting pattern for restarting a program when it fails.
+# Also, this redirects any program output into `stdout.txt` and `stderr.txt` files
+# for ease of debugging. 
 
 until [ sudo python3 /sensor_loop.py >> /stdout.txt 2> /stderr.txt ]
 do
